31b134adcea3fc21cc8f5b2fa2ec3d934b407757
sam <samuel_brolia@msn.com>
2019-02-18 10:28:46 +0000

    Combining into fewer files

--- create-all.bat
+++ create-all.bat
@@ -2,9 +2,6 @@ kubectl create -f monitoring-namespace.yaml
 
 kubectl create -f prometheus-config-map.yaml
 kubectl create -f prometheus-deployment.yaml
-kubectl create -f prometheus-service.yaml
 
 kubectl create -f pg-prometheus-config-map.yaml
-kubectl create -f pg-prometheus-storage.yaml
 kubectl create -f pg-prometheus-deployment.yaml
-kubectl create -f pg-prometheus-service.yaml
